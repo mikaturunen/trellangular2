5a5dacae505758eb685a92a1b702355160d3a1e4
Mika Turunen <mika.turunen@ymail.com>
2016-01-26 19:46:00 +0200

    STart working on shared modules.

---
+++ modules/log/log.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+export class Log {
+    constructor() {
+        // TODO Create logger
+    }
+
+    info(message: string) {
+        console.log(message);
+    }
+
+    debug(message: string) {
+        console.log(message);
+    }
+
+    warning(message: string) {
+        console.log(message);
+    }
+
+    error(message: string) {
+        console.log(message);
+    }
+};
---
+++ modules/release/modules/log/log.js
@@ -0,0 +1,19 @@
+"use strict";
+class Log {
+    constructor() {
+    }
+    info(message) {
+        console.log(message);
+    }
+    debug(message) {
+        console.log(message);
+    }
+    warning(message) {
+        console.log(message);
+    }
+    error(message) {
+        console.log(message);
+    }
+}
+exports.Log = Log;
+;
