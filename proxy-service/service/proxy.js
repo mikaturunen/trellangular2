e45cb17716b3985f9346f4689d5e4d5f4d1ac9bb
Mika Turunen <mika.turunen@ymail.com>
2016-01-26 19:54:35 +0200

    Create proxy service that starts instance of the logger. Push forward.

---
+++ proxy-service/service/proxy.js
@@ -0,0 +1,3 @@
+"use strict";
+var log_1 = require("../../modules/log/log");
+const log = new log_1.Log();
--- proxy-service/service/proxy.ts
+++ proxy-service/service/proxy.ts
@@ -1,3 +1,5 @@
 "use strict";
 
-import { Log } from "../../modules/release/modules/log/log";
+import { Log } from "../../modules/log/log";
+
+const log = new Log();
