47dd65951aa750d374b7d619682f2bd5d1c4218c
Mika Turunen <mika.turunen@ymail.com>
2016-01-27 19:44:02 +0200

    Update proxy service and compilation.

--- proxy-service/service/proxy.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var log_1 = require("../../modules/log/log");
-const log = new log_1.Log();
--- proxy-service/service/proxy.ts
+++ proxy-service/service/proxy.ts
@@ -1,5 +1,18 @@
 "use strict";
 
+import "babel-polyfill";
+import "babel-register";
+
+import * as express from "express";
 import { Log } from "../../modules/log/log";
 
 const log = new Log();
+const app = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    log.info("Example app listening on port 3000!");
+});
