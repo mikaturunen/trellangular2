605f56f602e2c306d317bc64ad7608fc844c4890
Mika Turunen <mika.turunen@ymail.com>
2016-01-31 22:15:32 +0200

    Start HTTP and the seneca client properly. Geesh. That was more difficult than it had to be..

--- services/web/web.js
+++ services/web/web.js
@@ -1,22 +1,18 @@
 "use strict";
 var express = require("express");
 var path = require("path");
-var Q = require("q");
-var parser = require("body-parse");
-var serve_static_1 = require("serve-static");
+var parser = require("body-parser");
 const seneca = require("seneca")();
 const configuration = require("../../config.json");
 const httpServer = express();
-const deferred = Q.defer();
-seneca
-    .use("../../lib/log")
-    .client(configuration.service.port.task)
-    .ready(() => deferred.resolve(true));
-deferred.promise
-    .done(() => {
-    httpServer.use(parser.json());
-    httpServer.use(serve_static_1.default("/bower", path.join(__dirname, "../../frontend/bower")));
-    httpServer.use(serve_static_1.default("/components", path.join(__dirname, "../../frontend/")));
-    httpServer.get("/", (request, response) => response.sendFile(path.join(__dirname, "../../frontend/index.html")));
-    seneca.act({ role: "log", cmd: "info", message: "" });
+httpServer.use(parser.json());
+httpServer.use("/bower", express.static(path.join(__dirname, "../../frontend/bower")));
+httpServer.use("/components", express.static(path.join(__dirname, "../../frontend/")));
+httpServer.get("/", (request, response) => response.sendFile(path.join(__dirname, "../../frontend/index.html")));
+httpServer.listen(9000, () => {
+    seneca
+        .client(configuration.service.port.task)
+        .ready(console.log);
+    seneca.act({ role: "log", cmd: "info", message: "TEST :D" });
 });
+console.log("test");
--- services/web/web.ts
+++ services/web/web.ts
@@ -2,29 +2,26 @@
 
 import * as express     from "express";
 import * as path        from "path";
-import * as Q           from "q";
-import * as parser      from "body-parse";
-import staticServe      from "serve-static";
+import * as parser      from "body-parser";
+// import * as staticServe      from "serve-static";
 
 const seneca: any = require("seneca")();
 const configuration: any = require("../../config.json");
 const httpServer = express();
 
-const deferred = Q.defer<boolean>();
+httpServer.use(parser.json());
+httpServer.use("/bower", express.static(path.join(__dirname, "../../frontend/bower")));
+httpServer.use("/components", express.static(path.join(__dirname, "../../frontend/")));
 
-seneca
-    .use("../../lib/log")
-    .client(configuration.service.port.task)
-    .ready(() => deferred.resolve(true));
+httpServer.get("/", (request: express.Request, response: express.Response) =>
+    response.sendFile(path.join(__dirname, "../../frontend/index.html"))
+);
 
-deferred.promise
-    .done(() => {
-        httpServer.use(parser.json());
-        httpServer.use(staticServe("/bower", path.join(__dirname, "../../frontend/bower")));
-        httpServer.use(staticServe("/components", path.join(__dirname, "../../frontend/")));
+httpServer.listen(9000, () => {
+    seneca
+        .client(configuration.service.port.task)
+        .ready(console.log);
 
-        httpServer.get("/", (request: express.Request, response: express.Response) =>
-            response.sendFile(path.join(__dirname, "../../frontend/index.html"))
-        );
-        seneca.act({ role: "log", cmd: "info", message: "" });
-    });
+    seneca.act({ role: "log", cmd: "info", message: "TEST :D" })
+});
+console.log("test");
